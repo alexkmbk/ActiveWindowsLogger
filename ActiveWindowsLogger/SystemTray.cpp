6c47fc8f5f6256e8c23236f8478d5fe326871c30
alexkmbk <alexkmbk@gmail.com>
2022-08-07 21:30:25 +0600

    WinSetup improvements

--- ActiveWindowsLogger/SystemTray.cpp
+++ ActiveWindowsLogger/SystemTray.cpp
@@ -8,7 +8,7 @@
 #ifdef _DEBUG 
 class __declspec(uuid("C805DD46-663F-426C-8C47-95EF8AB5BFE0")) SystemTrayIconUUID;
 #else 
-class __declspec(uuid("87C9AEE8-17BD-408A-A448-3AE7C83AFB22")) SystemTrayIconUUID;
+class __declspec(uuid("7E17ADC0-2E82-4491-9B9B-4E9B99CCE5B2")) SystemTrayIconUUID;
 #endif
 
 BOOL AddNotificationIcon(HWND hwnd, HINSTANCE hInstance)
@@ -18,7 +18,7 @@ BOOL AddNotificationIcon(HWND hwnd, HINSTANCE hInstance)
 	// add the icon, setting the icon, tooltip, and callback message.
 	// the icon will be identified with the GUID
 	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE | NIF_SHOWTIP | NIF_GUID;
-	//nid.guidItem = __uuidof(SystemTrayIconUUID);
+	nid.guidItem = __uuidof(SystemTrayIconUUID);
 	nid.uCallbackMessage = WMAPP_NOTIFYCALLBACK;
 	LoadIconMetric(hInstance, MAKEINTRESOURCE(IDI_SMALL), LIM_SMALL, &nid.hIcon);
 	LoadString(hInstance, IDS_APP_TITLE, nid.szTip, ARRAYSIZE(nid.szTip));
@@ -27,14 +27,20 @@ BOOL AddNotificationIcon(HWND hwnd, HINSTANCE hInstance)
 
 	// NOTIFYICON_VERSION_4 is prefered
 	nid.uVersion = NOTIFYICON_VERSION_4;
-	return Shell_NotifyIcon(NIM_SETVERSION, &nid);
+	bool res = Shell_NotifyIcon(NIM_SETVERSION, &nid);
+
+	if (!res) {
+		nid.guidItem = GUID();
+		res = Shell_NotifyIcon(NIM_SETVERSION, &nid);
+	}
+	return res;
 }
 
 BOOL DeleteNotificationIcon()
 {
 	NOTIFYICONDATA nid = { sizeof(nid) };
 	nid.uFlags = NIF_GUID;
-	//nid.guidItem = __uuidof(SystemTrayIconUUID);
+	nid.guidItem = __uuidof(SystemTrayIconUUID);
 	return Shell_NotifyIcon(NIM_DELETE, &nid);
 }
 
@@ -71,7 +77,7 @@ BOOL ShowLowInkBalloon(HINSTANCE hInstance)
 	// Display a low ink balloon message. This is a warning, so show the appropriate system icon.
 	NOTIFYICONDATA nid = { sizeof(nid) };
 	nid.uFlags = NIF_INFO | NIF_GUID;
-	//nid.guidItem = __uuidof(SystemTrayIconUUID);
+	nid.guidItem = __uuidof(SystemTrayIconUUID);
 	// respect quiet time since this balloon did not come from a direct user action.
 	nid.dwInfoFlags = NIIF_WARNING | NIIF_RESPECT_QUIET_TIME;
 	//LoadString(hInstance, IDS_APP_TITLE, nid.szInfoTitle, ARRAYSIZE(nid.szInfoTitle));
--- WinSetup/make.bat
+++ WinSetup/make.bat
@@ -1,2 +1,4 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" -ext WixUIExtension -ext WixUtilExtension ActiveWindowsLogger.wxs
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUIExtension -ext WixUtilExtension ActiveWindowsLogger.wixobj
+del ActiveWindowsLogger.wixobj
+del ActiveWindowsLogger.wixpdb
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" -ext WixUtilExtension ActiveWindowsLogger.wxs
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUtilExtension ActiveWindowsLogger.wixobj
